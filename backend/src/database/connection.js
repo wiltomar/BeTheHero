c5b464c44ded064e4f6eac454a163569c0c85d80
Wiltomar DUARTE <wiltomar@yahoo.com.br>
2020-03-29 22:10:35 -0300

    Atualizações do projeto todo.

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,6 +1,8 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
 
-const connection = knex(configuration.development);
+const connfig = process.env.NODE_ENV ? configuration.test : configuration.development;
+
+const connection = knex(connfig);
 
 module.exports = connection;
